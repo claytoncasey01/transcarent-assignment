ef7c63e56e404dcc23dfc988ded12739e93e4b96
Casey Clayton <claytoncasey01@gmail.com>
2021-06-12 04:34:12 -0400

    Router package

---
+++ server/router/router_test.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"testing"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Make sure we get a new Echo.
+func TestNew(t *testing.T) {
+	var i interface{} = New()
+
+	_, ok := i.(*echo.Echo)
+
+	if !ok {
+		t.Fatalf("Did not recieve the expected type (*echo.Echo)")
+	}
+}
